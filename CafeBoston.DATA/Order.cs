057757aea252a133a64dbe56653234a0f482214d
Erhan <erhangokceoglu91@gmail.com>
2022-10-18 13:55:14 +0300

    Class added

---
+++ CafeBoston.DATA/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeBoston.DATA
+{
+    public class Order
+    {
+        public int TableNo { get; set; }
+
+        public OrderState State { get; set; }
+
+        public decimal PaidAmount { get; set; }
+
+        public DateTime StartTime { get;  } = DateTime.Now; 
+
+        public DateTime EndTime { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+
+        public readonly string TotalPriceTRY;
+
+        public decimal TotalPrice()
+        {
+            return 0;
+        }
+    }
+}
